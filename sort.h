bae3502bcb9b5855fbcb3779fca429f8642d7fc1
M33x0 <michael.mbea@gmail.com>
2021-09-13 18:57:46 +0200

    added function

--- sort.h
+++ sort.h
@@ -29,4 +29,5 @@ void shell_sort(int *array, size_t size);
 void swap(int *a, int *b);
 void counting_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
 #endif
